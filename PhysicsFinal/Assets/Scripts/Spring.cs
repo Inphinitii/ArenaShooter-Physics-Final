8fde2c22fe7fb4507d84c0f9c9e4ac8c9b659a14
Watts1234 <d.e.watts1234@gmail.com>
2014-03-13 15:52:19 -0400

    Spring script, attach the 2 objects you want to be attached by a spring to the spring points

---
+++ PhysicsFinal/Assets/Scripts/Spring.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spring : MonoBehaviour {
+	//The two points that the spring connects to
+	public Transform springPoint1;
+	public Transform springPoint2;
+
+	//spring constant is a measure of the elasticity of the spring 
+	//The higher its value, the more the force you will need to exert 
+	//to extend the spring. 
+	public float konstant;
+
+	//damper to slow the spring down over time
+	public float damper;
+
+	//the size of the spring at rest
+	public float springSize;
+	
+	private float mag;
+	private Vector3 dist;
+	private Vector3 springForce;
+	private Vector3 damperForce;
+	private Vector3 accel;
+	private Vector3 netForce;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		dist = (springPoint2.position - springPoint1.position);
+
+		mag = Mathf.Sqrt((dist.x * dist.x) + (dist.y * dist.y) + (dist.z * dist.z));
+		Vector3 normalizedDist = dist / mag;
+
+		springForce = konstant * (dist - (normalizedDist * springSize));
+		damperForce = damper * (springPoint2.rigidbody.velocity - springPoint1.rigidbody.velocity);
+
+		netForce += damperForce + springForce;
+
+		accel = netForce / springPoint1.rigidbody.mass;
+
+		springPoint1.rigidbody.velocity += accel * Time.deltaTime;
+
+		//Make sure to reset the net force
+		netForce = new Vector3(0,0,0);
+	}
+}
