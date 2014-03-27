5147b33c077ad4713930032cbab9b89ceedbba0d
solsnare <solsnare@gmail.com>
2014-03-27 17:32:16 -0400

    First character animations added (uses physics and the movement controller to determine animation outcome)

---
+++ PhysicsFinal/Assets/Resources/Utility Scripts/PhysicsAnimationController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsAnimationController : MonoBehaviour {
+
+	// Use this for initialization
+    public Animator currentAnimator;
+    public Movement movement;
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentAnimator.SetFloat("VelocityY", rigidbody.velocity.y);
+        currentAnimator.SetFloat("VelocityX", rigidbody.velocity.x);
+        currentAnimator.SetFloat("AbsoluteVelocityY", Mathf.Abs(rigidbody.velocity.y));
+        currentAnimator.SetFloat("AbsoluteVelocityX", Mathf.Abs(rigidbody.velocity.x));
+        currentAnimator.SetBool("isGrounded", movement.Grounded);
+	}
+}
